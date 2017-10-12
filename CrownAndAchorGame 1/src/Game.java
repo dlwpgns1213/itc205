dc07ff650ae21fbfbabad6a3ad330106a0a2411c
dlwpgns1213 <30452048+dlwpgns1213@users.noreply.github.com>
2017-10-12 21:44:57 +1100

    bug 1 completly fixed

--- CrownAndAchorGame 1/src/Game.java
+++ CrownAndAchorGame 1/src/Game.java
@@ -42,7 +42,8 @@ public class Game {
 		int winnings = matches * bet;
 
 		if (matches > 0) {			
-			player.receiveWinnings(winnings);
+			player.receiveWinnings(winnings + bet
+                        );
 		}
         return winnings;		
 	}
